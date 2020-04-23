055e8648c60c528c2f30100383983f875e61ef72
tkt96 <trankhanhtoan96@gmail.com>
2020-04-23 14:42:46 +0700

    tkt

--- selenium-server.sh
+++ selenium-server.sh
@@ -7,7 +7,7 @@ pip3 install selenium
 pip3 install pyvirtualdisplay
 apt-get -y install xvfb
 cd /root
-git clone https://trankhanhtoan96:Trankhanhtoan29101996@github.com/tkt/selenium.git
+git clone https://trankhanhtoan96:Trankhanhtoan29101996@github.com/tkt96/selenium.git
 chmod +x /root/selenium/firefox_driver/geckodriver-linux64
 apt-get -y update
 echo "=========DONE==========\n"
